56911e6db1790608d763044c83d219fb7dd3e614
karthik nayak <karunayak63@gmail.com>
2022-01-24 18:15:59 +0530

    feat: add firebase functions
    * firebase initialization
    * firebase auth functions

---
+++ src/firebase/firebase-auth.js
@@ -0,0 +1,18 @@
+import { auth } from "./firebase";
+import {
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+export const signInWithEmail = ({ email, password }) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const createUser = ({ email, password }) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const userSignOut = () => {
+  return signOut(auth);
+};
