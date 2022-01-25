035d70d779eeaa4efa1b67d5c1761e90735ad204
karthik nayak <karunayak63@gmail.com>
2022-01-25 20:52:33 +0530

    feat: add header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header({ title }) {
+  return (
+    <h2 className='text-center text-pink-400 text-2xl font-bold'>{title}</h2>
+  );
+}
+
+export default Header;
