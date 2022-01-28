c581588d17453263c57bc6ef03df50224621739e
karthik nayak <karunayak63@gmail.com>
2022-01-28 22:12:14 +0530

    feat: add routes, context, navbar to file

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+
+import AuthContextProvider from "../context/authcontext";
+
+import HomePage from "./home/homepage";
+import SignUp from "../pages/signup";
+import SignIn from "../pages/signin";
+import Navbar from "./navbar/navbar.component";
+import EditProfile from "../pages/editprofile";
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <AuthContextProvider>
+      <div className='App'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/editprofile' element={<EditProfile />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </div>
+    </AuthContextProvider>
+  );
 }
 
 export default App;
