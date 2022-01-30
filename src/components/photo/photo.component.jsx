83c0070bda967917b54816303d4284c3d2485dfa
karthik nayak <karunayak63@gmail.com>
2022-01-30 22:42:04 +0530

    feat: component to display each photo.

--- src/components/photo/photo.component.jsx
+++ src/components/photo/photo.component.jsx
@@ -1,11 +1,84 @@
-import React from "react";
-import { BsFillHeartFill, BsHeart } from "react-icons/bs";
+import {
+  arrayRemove,
+  arrayUnion,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+import React, { useContext, useState } from "react";
+import { BsHeartFill, BsHeart } from "react-icons/bs";
+import { MdDelete } from "react-icons/md";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authcontext";
+import { db, storage } from "../../firebase/firebase";
 
-function Photo({ id, imageUrl, displayName = "", photoURL = "" }) {
+function Photo({
+  uid,
+  id,
+  imageUrl,
+  imageName,
+  displayName = "",
+  photoURL = "",
+  likeCount,
+  isLiked,
+}) {
+  const { user } = useContext(AuthContext);
+  const [like, setLike] = useState(isLiked);
+  const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    const imagesDoc = doc(db, "images-info", id);
+    const storageRef = ref(storage, `images/${imageName}`);
+
+    // Delete the file
+    try {
+      await deleteObject(storageRef);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // Delete document from firestore
+    try {
+      await deleteDoc(imagesDoc);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDislike = async () => {
+    if (!user?.uid) navigate("/signin");
+
+    setLike(false);
+
+    const imageDocRef = doc(db, "images-info", id);
+    const usersDocRef = doc(db, "users", user?.uid);
+
+    if (likeCount > 0) likeCount = likeCount - 1;
+    else likeCount = 0;
+
+    await updateDoc(imageDocRef, { likeCount: likeCount });
+    await updateDoc(usersDocRef, {
+      likes: arrayRemove(id),
+    });
+  };
+
+  const handleLike = async () => {
+    if (!user?.uid) navigate("/signin");
+
+    setLike(true);
+    const imageDocRef = doc(db, "images-info", id);
+    const usersDocRef = doc(db, "users", user?.uid);
+    await updateDoc(imageDocRef, { likeCount: likeCount + 1 });
+    await updateDoc(usersDocRef, {
+      likes: arrayUnion(id),
+    });
+  };
+
   return (
     <div
       key={id}
-      className='shadow-lg w-full shadow-slate-300 rounded-lg sm:w-56 my-3'>
+      className='shadow-lg w-full shadow-slate-300 rounded-lg sm:w-56 my-3 h-72'>
       <figure className='h-60'>
         <img
           className='p-1 object-cover h-60 mx-auto'
@@ -20,15 +93,46 @@ function Photo({ id, imageUrl, displayName = "", photoURL = "" }) {
             src={photoURL}
             alt={displayName}
           />
-          <p>{displayName}</p>
+          <Link
+            to='/profile'
+            state={{
+              name: displayName,
+              imageUrl: photoURL,
+              uid: uid,
+            }}>
+            <p className='cursor-pointer'>{displayName}</p>
+          </Link>
         </div>
         <p className='flex items-center gap-1'>
-          {/* <BsFillHeartFill
-                    className='mt-1 text-red-500'
-                    size='1.2rem'
-                  />{" "} */}
-          <BsHeart className='mt-1 text-red-500' size='1.2rem' />
-          18
+          <>
+            {user?.uid === uid ? (
+              <MdDelete
+                size='1.3rem'
+                className='text-red-500 cursor-pointer'
+                onClick={handleDelete}
+              />
+            ) : null}
+
+            {isLiked ? (
+              <>
+                <BsHeartFill
+                  onClick={handleDislike}
+                  className='cursor-pointer mt-1 text-red-500'
+                  size='1.2rem'
+                />
+                {likeCount}
+              </>
+            ) : (
+              <>
+                <BsHeart
+                  onClick={handleLike}
+                  className='cursor-pointer mt-1 text-red-500'
+                  size='1.2rem'
+                />
+                {likeCount}
+              </>
+            )}
+          </>
         </p>
       </figcaption>
     </div>
